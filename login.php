eeaabe6147790e8cb1f0e874199d7dc3a33954bb
ViljarLaasi <viljarlaasi@gmail.com>
2016-01-12 15:03:46 +0200

    Parandused eelnevale

--- login.php
+++ login.php
@@ -7,49 +7,54 @@
 	$password_error = "";
 	$name_error = "";
 	$phone_error = "";
+	$email_error1 = "";
+	$password_error1 = "";
 	
 	// kontrollime et keegi vajutas input nuppu
-	if($_SERVER["REQUEST_METHOD"] == "POST") {
-		
+	//if($_SERVER['REQUEST_METHOD'] == 'POST') {
+		//sign up errorid
+		if(isset($_POST['sign_up'])){
 		//echo "keegi vajutas nuppu";
-		
 		//kontrollin et e-post ei ole tühi
-		if ( empty($_POST["name"]) ) {
+		if ( empty($_POST['name']) ) {
 			$name_error = "See väli on kohustuslik";
 		}
-		
 			if ( empty($_POST["phone"]) ) {
 			$phone_error = "See väli on kohustuslik";
-			
 		} else { 
-		if(is_numeric($_POST["phone"])){}
-			
+		if(is_numeric($_POST["phone"])){
+		}
 		else{
 			$phone_error = "siin peab olema number";
 			}
 		}
-		
 		if ( empty($_POST["email"]) ) {
 			$email_error = "See väli on kohustuslik";
 		}
-		
 		//kontrollin, et parool ei ole tühi
 		if ( empty($_POST["password"]) ) {
 			$password_error = "See väli on kohustuslik";
 		} else {
-			
 			// kui oleme siia jõudnud, siis parool ei ole tühi
 			// kontrollin, et oleks vähemalt 8 sümbolit pikk
 			if(strlen($_POST["password"]) < 8) { 
-			
 				$password_error = "Peab olema vähemalt 8 tähemärki pikk!";
-				
 			}
-			
 		}
-		
 	}
-	
+	//log_in errorid
+	if(isset($_POST['log_in'])){
+		if ( empty($_POST["email"]) ) {
+			$email_error1 = "See väli on kohustuslik";
+		}
+		if ( empty($_POST["password"]) ) {
+			$password_error1 = "See väli on kohustuslik";
+		} else {
+			if(strlen($_POST["password"]) < 8) { 
+				$password_error1 = "Peab olema vähemalt 8 tähemärki pikk!";
+			}
+		
+	}}
 ?>
 <html>
 <head>
@@ -60,18 +65,18 @@
 	<h2>Log in</h2>
 		
 		<form action="login.php" method="post" >
-			<input name="email" type="email" placeholder="E-post"> <?php echo $email_error; ?>
+			<input name="email" type="email" placeholder="E-post"> <?php echo $email_error1; ?>
 			<br><br>
-			<input name="password" type="password" placeholder="Parool"> <?php echo $password_error; ?> 
+			<input name="password" type="password" placeholder="Parool"> <?php echo $password_error1; ?> 
 			<br><br>
-			<input type="submit" value="Log in">
+			<input type="submit" value="Log in" name="log_in">
 		</form><br>
-	<a href="?create_user">Create User</a>
+	<a href=?create_user>Create User</a>
 	
 		<h2>Create User</h2>
-		<form action="login.php">
+		<form action="login.php"method="post"id="create_user">
 		
-			<input type="text" name="name" id="name" placeholder="Taisnimi"><?php echo $name_error; ?>
+			<input type="text" name="name" id="name" placeholder="Taisnimi" required /><?php echo $name_error; ?>
 			<br><br>
 			<input type="tel" name="phone" id="phone" placeholder="Telefoni nr."><?php echo $phone_error; ?>
 			<br><br>
@@ -84,7 +89,7 @@
 			<input type="password" name="password" id="password" placeholder="Parool" > <?php echo $password_error; ?> 
 			<br><br>
 			
-			<input type="submit" value="Sign up">
+			<input type="submit" value="Sign up" name="sign_up">
 		</form>
 	<text>
 	Tahan teha lehe mis aitaks kokku viima erinevaid priþi mängijaid.
